25974fcee10408217a1ba07fb407bf5b9daba648
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2016-03-19 22:53:06 +0100

    Redmine: read API key from env (hi 12factors)

--- gredmine.py
+++ gredmine.py
@@ -1,10 +1,20 @@
 from sopel import module
 from redmine import Redmine, ResourceNotFoundError
 import re
+from os import environ
 
 # Redmine
+envkey = environ.get('REDMINE_API_KEY')
+
+if envkey:  # Found Redmine API key in environment variables
+    apiKey = envkey
+else:       # Nothing in env vars, let's try key.txt file
+    try:
+        apiKey = open("key.txt", 'r').readline().rstrip('\n')
+    except:
+        raise Exception("can't find any Redmine API key, neither in env nor in key.txt")
+
 url     = "https://redmine.gconfs.fr"
-apiKey  = open("key.txt", 'r').readline().rstrip('\n')
 version = "3.2.0.stable"
 redmine = None
 
